45555cb8dfe1f279ba09179de52b5513971838ea
Gloria Guy <totallygloria@gmail.com>
2017-03-15 11:21:04 -0700

    Initial commit for the create-react-app project files
    Removes unnecessary logo and style files, renames App to match the
    project name ReactTransplantr, removes the default react demo code,
    puts the tests in a test directory, and adds the old project styles
    to the folder. Ready to start porting components next.

---
+++ app/src/ReactTransplantr.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class ReactTransplantr extends Component {
+  render() {
+    return (
+      <div>
+          <h2>Welcome to React</h2>
+          <p>To get started, edit</p>
+      </div>
+    );
+  }
+}
+
+
---
+++ app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTransplantr from './ReactTransplantr';
+import './index.css';
+
+ReactDOM.render(
+  <ReactTransplantr />,
+  document.getElementById('root')
+);
