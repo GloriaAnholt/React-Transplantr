7dbec93f50b4f4ab5c19af41ce2384ab9a0d7682
Gloria Guy <totallygloria@gmail.com>
2017-03-15 11:52:03 -0700

    Refactors component to use ternary logic for current/dest city
    The text at the bottom of the component should change to respond to
    whether it refers to the left or right side of the comparison
    (current city vs destination city). This incorporates that check as
    a check on currentCity on props to return the html for that case.

--- app/src/components/IncomeDisplay.js
+++ app/src/components/IncomeDisplay.js
@@ -9,15 +9,30 @@ export default function IncomeDisplay(props) {
             <p>Median Income: <strong>{ props.medianIncome }</strong> (household)</p>
             <p>Federal Median Income: <strong>$56516</strong> (household)</p>
             <p>Federal Minimum: <strong>$15080</strong> (individual)</p>
-            <p>{ props.county } Percentage Poverty: <strong>{ props.percentPoverty }</strong></p>
-            <p>We get our median income and poverty data from the U.S. Census Bureau's Small Area Income and Poverty
-                Estimates (SAIPE) program's 2012 census. The program provides estimates of income and poverty levels for
-                all school districts, counties, and states in the US.
-            </p>
-            <p> According to the SAIPE Census in 2012, in { props.county }, the median income was <strong>{ props.medianIncome }</strong>.</p>
-            <p><strong>{ props.percentPoverty }</strong> of people in the county were living at or below the Federal Poverty Line.</p>
+            <p>{ props.county } Percentage Poverty: <strong>{ props.percentPoverty }</strong></p>       
+            {  // check which set of text to return based on the side asking for the component
+                props.currentCity ? 
+                    <div>
+                        <p>We get our median income and poverty data from the U.S. Census Bureau's Small Area Income and Poverty
+                        Estimates (SAIPE) program's 2012 census. The program provides estimates of income and poverty levels for
+                        all school districts, counties, and states in the US.
+                        </p> 
+                        <p> According to the SAIPE Census in 2012, in { props.county }, the median income was <strong>{ props.medianIncome }</strong>.</p>
+                        <p><strong>{ props.percentPoverty }</strong> of people in the county were living at or below the Federal Poverty Line.</p>
+                    </div>
+                : 
+                    <p>Given your current income's relation to the median (in your current city), we've calculate
+                        what you would need to make in order to maintain the same relationship to the new median income.
+                        That is, to maintain approximately your lifestyle, this is how much do you need to make in your new city.
+                    </p> 
+            }
         </div>
     )
+
+    
+
+
+
  }
 
 IncomeDisplay.propTypes = {
