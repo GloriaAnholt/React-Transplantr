7500fb1d9d729044b69900947ada789e313d359a
Gloria Guy <totallygloria@gmail.com>
2017-03-15 16:35:59 -0700

    Adds a map for the incoming county data to make option dropdown
    The component should be handed data from an ajax call to the census
    API, once the data comes back, it should be mapped over to make the
    dropdow.
    
    As none of the data is there and the fetch isn't hooked in, this
    doesn't even slightly work.

--- app/src/components/CountyDropdown.js
+++ app/src/components/CountyDropdown.js
@@ -5,12 +5,19 @@ import '../../public/styles/modules.css';
 export default function CountyDropdown(props) {
 
     let LOCATION = (props.location === 'current') ? 'Current' : 'Destination';
-    console.log('location is', LOCATION);
+
+    let countyOptions = props.county.map((county, i) => {
+        return ( 
+            <option key={i} value={county}>{ county }</option> 
+        )
+    })
+
     return (
         <span>
             <label htmlFor="county-filter">{ LOCATION } County:</label>
                 <select className="dropdowns" id="county-filter">
                   <option value="">Choose County</option> 
+                  { countyOptions }
                 </select>
         </span>
     )
