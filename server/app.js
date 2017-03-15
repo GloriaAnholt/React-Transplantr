49cd1378409aab19db98a90afce200fb4cc80eb6
Gloria Guy <totallygloria@gmail.com>
2017-03-15 09:37:18 -0700

    Moves all of the express app out of the server into an app module
    The server start and the express app were both in the same file.
    This separates them into a server and an express router. The express
    router now takes in the proxy functions as app.use subrouters.
    
    Hasn't been tested, likely doesn't work.

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const proxyZillow = require('./proxy-zillow');
+const proxyCensus = require('./proxy-census');
+
+
+app.use(morgan(dev))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    console.log('Header', req.headers['x-forwarded-proto']);
+    if (req.headers['x-forwarded-proto'] === 'https') next(); 
+    else res.redirect(`https://${req.hostname}${req.url}`);
+  });
+}
+
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+app.use('/zillow/:state/:county', proxyZillow);
+app.use('/census/:state', proxyCensus);
+
+app.use(express.static('./'));
+
+app.get('*', (req, res) => {
+  console.log('New request:', request.url);
+  res.sendFile('index.html', {root: '.'});
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const proxyZillow = require('./proxy-zillow');
-const proxyCensus = require('./proxyCensus');
+const http = require('http');
+const app = require('./app');
 
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-app.use(morgan(dev))
-
-app.use('/zillow/:state/:county', proxyZillow);
-
-app.use('/census/:state', proxyCensus);
-
-// The serve-all has to be below the specific requests or it overrides them
-app.use(express.static('./'));
-
-app.get('*', function(req, res) {
-  console.log('New request:', request.url);
-  res.sendFile('index.html', {root: '.'});
-});
-
-app.listen(port, function() {
-  console.log('Server started on port ' + port + '!');
+server.listen(port, () => {
+  console.log('Server started on: ', server.address());
 });
