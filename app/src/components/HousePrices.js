8910d8e12ccdc8931044e2999bad00f4e42d44b5
Gloria Guy <totallygloria@gmail.com>
2017-03-15 11:58:30 -0700

    Refactors house prices handlebars into React component with ternary op
    Uses the same style of ternary operator to determine if the returned
    text should be for the current city or the destination city.

---
+++ app/src/components/HousePrices.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+
+
+export default function HousePrices(props) {
+    return (
+        <div>
+            <h3>House Prices</h3>
+            <p>Median House Price: <strong>{ props.aveHousePrice }</strong></p>
+            <p id="curr-income_to_mortgage"></p>
+            { 
+                props.currentCity ? 
+                    <p>We get our house price data from Zillow.com, an online superstore for buying and selling homes.
+                        According to Zillow, in { props.city } the <strong>median house price</strong> is <strong>{ props.aveHousePrice }</strong></p>
+                :
+                    <p>Given your current income, this is how much you would need to make in order to have the same
+                        home buying power in your new city.</p>
+            }
+        </div>
+    )
+}
+
+HousePrices.propTypes = {
+    aveHousePrice: PropTypes.number,
+    city: PropTypes.string,
+    currentCity: PropTypes.string.isRequired,
+}      
