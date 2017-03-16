ae26a9900148250fa8fdbddd690f934003964ad7
Gloria Guy <totallygloria@gmail.com>
2017-03-15 17:22:53 -0700

    Adds conditional logic for which classes to apply to the form
    The left side (current) is properly using the styles and everything
    is centered as intended. The right side won't center the dropdown
    menus. I've tried taking styles off and putting them back on, but
    I haven't found what is missing yet. Committing with the style bug.

--- app/src/components/CitySelector.js
+++ app/src/components/CitySelector.js
@@ -10,10 +10,11 @@ import '../../public/styles/modules.css';
 
 export default function CitySelector(props) {
     return (
-        <div className={ props.location }>
+        <div className={ props.location === 'current' ? "current-side" : "destination-side" }>
           <div id="data-page" className="tab-content">
             <div className="datacontainer">
-              <form id="#current-city-input">
+              <form className={ props.location === 'current' ? 
+                "city-input current-side" : "city-input destination-side" }>
                 { props.location === 'current' ? 
                   <h2>Your Current City</h2> : <h2>Destination City</h2>
                 }
@@ -28,7 +29,6 @@ export default function CitySelector(props) {
                     <input type="text" name="current-income" id="current-income" />
                   </span>  
                 }
-              
               </form>
             </div>
             <IncomeDisplay className="datacontainer" />
