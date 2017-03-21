e861f6e86db5e9e4c7acc410239344ae45f59014
Gloria Guy <totallygloria@gmail.com>
2017-03-21 13:05:45 -0700

    Moves hero banner code into a component.

---
+++ app/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+export default function Hero() {
+    return (
+        <section>
+            <div className="hero">
+                <h1 id="transplantr" class="tagline">
+                    Transplantr: The Smarter Way to Move
+                </h1>
+            </div> 
+      </section>
+    )
+}
