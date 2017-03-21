c26772cf4a1a5c1a355c210ab8081406016256c7
Gloria Guy <totallygloria@gmail.com>
2017-03-21 13:49:57 -0700

    Fixes bug in Hero - had class instead of className.

--- app/src/components/Hero.js
+++ app/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section>
             <div className="hero">
-                <h1 id="transplantr" class="tagline">
+                <h1 id="transplantr" className="tagline">
                     Transplantr: The Smarter Way to Move
                 </h1>
             </div> 
