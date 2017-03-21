cc4f1094dd837d40deadc063415ac868370b9e1d
Gloria Guy <totallygloria@gmail.com>
2017-03-21 13:06:11 -0700

    Moves landing text into a component. This needs to be a sibling to other views.

---
+++ app/src/components/Landing.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../index.css';
+
+export default function Landing() {
+    return (
+        <div id="threeColumnHome" className="tab-content">
+          <div className="home_column">
+            <h1>Who</h1>
+            <p>Every year, approximately 14% of Americans move. Whether it be to another part of their city or to another state entirely, over 40 million Americans are scouring the internet and other resources to find the information and tools they need to relocate.</p>
+            <p><a href="about">Learn more</a></p>
+          </div>
+          <div className="home_column">
+            <h1>What</h1>
+            <p><b>Transplantr</b> was established in 2016 with a mission to provide Americans with the information they need to plan their move. While a plethora of websites can provide this information, Transplantr serves to be an all-in-one resource.</p>
+            <p><a href="about">Learn more</a></p>
+          </div>
+          <div className="home_column">
+            <h1>Why</h1>
+            <p>Zillow, a popular real estate website, provides users with property listings in a given area but does not offer the average salary and cost of living in that area. Other sites  help users compare the average cost of living in their current city to their provided destination, but <b>Transplantr</b> is different.</p>
+            <p><a href="about">Learn more</a></p>
+          </div>
+          <div className="get-started">
+          <form action="city-data">
+            <input type="submit" className="get-started-btn" value="Get Started" />
+          </form>
+          </div>
+        </div>
+    )
+}
