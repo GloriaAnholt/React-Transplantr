b886a1d0e1b8adce26b019f8095198bc2e5dcaa1
Gloria Guy <totallygloria@gmail.com>
2017-03-23 14:24:43 -0700

    Adds a Link for the button to link to charts page.

--- app/src/components/CityData.js
+++ app/src/components/CityData.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import CitySelector from './CitySelector'
-
+import '../../public/styles/modules.css';
 
 export default class CityData extends Component {
   render() {
     return (
       <div>
+        <div>
           <CitySelector location={"current"} />
           <CitySelector location={"destination"} />
+        </div>
+
+          <div className="showChartContainer">
+            <Link to="chart-data">
+              <button className="get-started-btn" id="chart-submit">
+                Show Charts
+              </button>
+            </Link>
+          </div>
       </div>
     );
   }
